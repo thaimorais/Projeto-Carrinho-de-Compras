15e93fd08190a843888f42cd2be80c77004de913
Thaisy Morais <thaisy-ms@outlook.com>
2022-06-03 19:47:12 -0300

    requisito 3

--- helpers/fetchItem.js
+++ helpers/fetchItem.js
@@ -1,5 +1,11 @@
-const fetchItem = () => {
-  // seu código aqui
+const fetchItem = async (item) => {
+  try {
+    const url = await fetch(`https://api.mercadolibre.com/items/${item}`);
+    const response = await url.json();
+    return response;
+  } catch (error) {
+    return error;
+  }
 };
 
 if (typeof module !== 'undefined') {
--- tests/fetchItem.test.js
+++ tests/fetchItem.test.js
@@ -3,6 +3,20 @@ const { fetchItem } = require('../helpers/fetchItem');
 const item = require('../mocks/item');
 
 describe('2 - Teste a função fetchItem', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  test('Verifica se fetchItem é uma função', () => {
+    expect(typeof fetchItem).toBe('function');
+  });
+  test('Verifica se fetch é chamada quando passado o argumento /MLB1615760527/', async () => {
+    expect.assertions(2);
+    await fetchItem('MLB1615760527');
+    const url = 'https://api.mercadolibre.com/items/MLB1615760527';
+    expect(fetch).toHaveBeenCalled();
+    expect(fetch).toHaveBeenCalledWith(url);
+  });
+  test('Verifica se a estrutura é igual a /item/ quando passado o argumento /MLB1615760527/', async () => {
+    expect(fetchItem('MLB1615760527')).resolves.toEqual(item);
+  });
+  test('Verifica se a mensagem de erro aparece ao não passar argumento', async () => {
+    await expect(fetchItem()).resolves.toThrow(new Error('You must provide an url'));
+  });
 });
